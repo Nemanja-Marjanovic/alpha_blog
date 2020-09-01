29d5beb8a9be653fcf2048c5e37e686ced83b5f2
Nemanja Marjanovic <mr.nemanja.marjanovic@gmail.com>
2020-09-01 12:39:14 +0200

    Cateogry model and testing

---
+++ test/models/category_test.rb
@@ -0,0 +1,10 @@
+require 'test_helper'
+
+class CategoryTest < ActiveSupport::TestCase
+
+  test "cateogry should be valid" do
+    @category = Category.new(name: "Sports")
+    assert @category.valid?
+  end
+
+end
