9ab7375aac08930e9f80194bfe57094586b4fbfa
Nemanja Marjanovic <mr.nemanja.marjanovic@gmail.com>
2020-09-02 10:29:42 +0200

    Create Category and test

--- test/controllers/categories_controller_test.rb
+++ test/controllers/categories_controller_test.rb
@@ -1,7 +1,7 @@
 require 'test_helper'
 
 class CategoriesControllerTest < ActionDispatch::IntegrationTest
-  
+
   setup do
     @category = Category.create(name: "Sports")
   end
@@ -16,14 +16,14 @@ class CategoriesControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
   end
 
-  # $test "should create category" do
-  #   assert_difference('Category.count') do
-  #     post categories_url, params: { category: {  } }
-  #   end
-  #
-  #     assert_redirected_to category_url(Category.last)
-  #   end
-  #
+  test "should create category" do
+    assert_difference('Category.count', 1) do
+      post categories_url, params: { category: { name: "Travel" } }
+    end
+
+    assert_redirected_to category_url(Category.last)
+  end
+
   test "should show category" do
     get category_url(@category)
     assert_response :success
